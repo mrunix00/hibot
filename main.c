1ebb30d9ff2478d657446333b5cc8db31439bf2c
mrunix00 <99476714+mrunix00@users.noreply.github.com>
2022-07-24 09:54:27 +0000

    Some improvements

--- main.c
+++ main.c
@@ -1,73 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 
-#include <unistd.h>
-
-#define maxBufferSize 1000
-#define maxWordSize 100
+#define maxLineSize 1000
 
 int InitConnection(int *fd, char *server, char *port);
-int divideSTR(char *str,int strlen,char **dest);
-int findSTR(char **str,int len,char *target);
+int findSTR(char *str,char *target,int tlen);
 
 int main(int argc, char *argv[]){
 	struct addrinfo hints,*res;
-	int fd,error;
-	if(argc != 2){
-		printf("Usage: %s <irc room>\n",argv[0]);
+	int fd=0,error=0;
+	char c,buff[maxLineSize];
+
+	if(argc != 3 || strlen(argv[1]) > 60){
+		printf("Usage: hibot <irc-server> <irc-room>\n");
 		return EXIT_FAILURE;
 	}
-	if((error=InitConnection(&fd,NULL,"6667"))>=0)
+
+	if((error=InitConnection(&fd,argv[1],"6667")) >= 0)
 		printf("CONNECTED!\n");
 	else{
-		printf("ERROR: %d\n",error);
+		perror(strerror(error));
 		return EXIT_FAILURE;
 	}
 
 	char login[]="NICK hibot\nUSER hibot 0 * :realname\n";
 	write(fd,login,sizeof(login));
 
-	char c,buff[maxBufferSize][maxWordSize];
-
-	int i=0,j=0;
-	while(read(fd,&c,sizeof(c))){
+	char join[70],hello[100],welcome[]="End of message of the day.";
+	sprintf(join,"JOIN #%s\n",argv[2]);
+	sprintf(hello,"PRIVMSG #%s :Hello!!!\n",argv[2]);
 	
+	int i=0,j=0;
+	while(read(fd,&c,sizeof(char)) != EOF){
+//		putchar(c);
+		buff[i]=c;
+		i++;
 		if(c == '\n'){
-			buff[i][j]='\0';
-//			printf("%s\n",buff[i]);
-			
-			char **temp=malloc(maxBufferSize*sizeof(char));
-			for(int l=0;l<maxBufferSize;l++) temp[l]=malloc(maxWordSize*sizeof(char));
-
-			int tmpINT=divideSTR(buff[i],strlen(buff[i]),temp);
-
-			if((findSTR(temp,--tmpINT,":Hi") != -1)){
-				char hi[100];
-				sprintf(hi,"PRIVMSG #%s :Hello!\n",argv[1]);
-				write(fd,hi,sizeof(hi));
-			}
-
-			free(temp);
-			for(int l=0;l<maxBufferSize;l++) free(temp[l]);
-
-			i++;
-			j=0;
-		}else{
-			
-			if((strcmp(buff[i],":unknown.local 376 hibot :End of message of the day.")) == 0){
+			if(findSTR(buff,welcome,strlen(welcome)) != -1){
 				printf("LOGGED IN!!\n");
-				char join[100];
-				sprintf(join,"JOIN #%s\n",argv[1]);
 				write(fd,join,sizeof(join));
 			}
-
-			buff[i][j++]=c;
+			if(findSTR(buff,":Hi",strlen(":Hi")) != -1) write(fd,hello,strlen(hello));
+			buff[0]='\0';
+			i=0;
 		}
 	}
 }
@@ -88,27 +71,16 @@ int InitConnection(int *fd,char *ip, char *port){
 	return err;
 }
 
-int divideSTR(char *str,int strlen ,char **dest){
-	int j=0,k=0;
+int findSTR(char *str,char *target,int tlen){
+	int i=0,j=0,k=0;
 
-	for(int i=0; i <= strlen; i++){
-		 if(str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\0' || str[i] == '\r'){
-			dest[j++][k]='\0';
-			k=0;
-		 }else{
-			dest[j][k++]=str[i];
-		 }
-	}
-	
-	return j;
-}
-
-int findSTR(char **str,int len,char *target){
-	for(int g=0;g < len;g++){
-		if(strcmp(str[g],target) == 0){
-			return g;
+	while(str[i] != '\0'){
+		if(str[i] == target[j]){
+			k=i;
+			while(target[j] != '\0' && target[j] == str[k]) {k++;j++;}
+			if(j == tlen) return i;
 		}
+		i++;j=0;
 	}
-
 	return -1;
 }
