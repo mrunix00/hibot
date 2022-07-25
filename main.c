644878fe370db69b6fded42492901310171d5259
mrunix00 <99476714+mrunix00@users.noreply.github.com>
2022-07-25 20:34:05 +0000

    small changes

--- main.c
+++ main.c
@@ -1,60 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
+
 #include <arpa/inet.h>
+#include <netdb.h>
+
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 #define maxLineSize 1000
 
-int InitConnection(int *fd, char *server, char *port);
-int findSTR(char *str,char *target,int tlen);
+int	InitConnection(int *fd, char *server, char *port);
+int	findSTR(char *str,char *target,int tlen);
 
+/*
+ * An simple IRC bot that says "Hello" to people when
+ * they say "Hi" in a specific chat room
+*/
 int main(int argc, char *argv[]){
 	struct addrinfo hints,*res;
 	int fd=0,error=0;
+	bool lstatus=false;
 	char c,buff[maxLineSize];
 
-	if(argc != 3 || strlen(argv[1]) > 60){
-		printf("Usage: hibot <irc-server> <irc-room>\n");
-		return EXIT_FAILURE;
-	}
+	if(argc != 3 || strlen(argv[1]) > 60){ printf("Usage: hibot <irc-server> <irc-room>\n"); return EXIT_FAILURE; }
 
-	if((error=InitConnection(&fd,argv[1],"6667")) >= 0)
-		printf("CONNECTED!\n");
-	else{
-		perror(strerror(error));
-		return EXIT_FAILURE;
-	}
+	if(InitConnection(&fd,argv[1],"6667") != -1) printf("CONNECTED!\n");
+	else perror("ERROR: ");
 
 	char login[]="NICK hibot\nUSER hibot 0 * :realname\n";
 	write(fd,login,sizeof(login));
 
-	char join[70],hello[100],welcome[]="End of message of the day.";
+	char join[70],hello[100],welcome[]="End of message of the day";
 	sprintf(join,"JOIN #%s\n",argv[2]);
 	sprintf(hello,"PRIVMSG #%s :Hello!!!\n",argv[2]);
 	
-	int i=0,j=0;
-	while(read(fd,&c,sizeof(char)) != EOF){
-//		putchar(c);
-		buff[i]=c;
-		i++;
+	int i=0;
+	while(read(fd,&c,sizeof(char))){
+		putchar(c);
+		buff[i++]=c;
 		if(c == '\n'){
-			if(findSTR(buff,welcome,strlen(welcome)) != -1){
+			if(findSTR(buff,welcome,strlen(welcome)) && lstatus == false){
+				lstatus = true;
 				printf("LOGGED IN!!\n");
 				write(fd,join,sizeof(join));
 			}
-			if(findSTR(buff,":Hi",strlen(":Hi")) != -1) write(fd,hello,strlen(hello));
+			if(findSTR(buff,":Hi",strlen(":Hi"))) write(fd,hello,strlen(hello));
 			buff[0]='\0';
 			i=0;
 		}
 	}
 }
 
+/*
+ * Connect to the server
+*/
 int InitConnection(int *fd,char *ip, char *port){
 	struct addrinfo hints,*res;
 	int err;
@@ -66,21 +69,24 @@ int InitConnection(int *fd,char *ip, char *port){
 
 	getaddrinfo(ip,"6667",&hints,&res);
 	*fd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-	err=connect(*fd,res->ai_addr,res->ai_addrlen);
+	err = connect(*fd,res->ai_addr,res->ai_addrlen);
 
 	return err;
 }
 
+/*
+ * Look for the string "target" in "str"
+ * if it was found return 1, if not return 0
+*/
 int findSTR(char *str,char *target,int tlen){
-	int i=0,j=0,k=0;
+	int i=0,j=0;
 
 	while(str[i] != '\0'){
 		if(str[i] == target[j]){
-			k=i;
-			while(target[j] != '\0' && target[j] == str[k]) {k++;j++;}
-			if(j == tlen) return i;
-		}
-		i++;j=0;
+			while(target[j] != '\0' && target[j] == str[i]){i++;j++;}
+			if(j == tlen) return 1;
+		}else i++;
+		j=0;
 	}
-	return -1;
+	return 0;
 }
